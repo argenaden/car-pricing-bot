1a0bceff3c50173908b592b5d8975c48a67fb266
argen <argen.adenov@emoldino.com>
2023-12-04 23:06:56 +0900

    update

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Fetch and save car data.")
     parser.add_argument("--manufacturer", type=str, default="기아", help="Manufacturer name")
     parser.add_argument("--category", type=str, default="경차", help="Car category")
-    parser.add_argument("--year_from", type=str, default="201000", help="Starting year")
+    parser.add_argument("--year_from", type=str, default="201400", help="Starting year")
     parser.add_argument("--year_to", type=str, default="202399", help="Ending year")
-    parser.add_argument("--page_count", type=int, default=30, help="Number of pages to fetch")
+    parser.add_argument("--page_count", type=int, default=10, help="Number of pages to fetch")
     return parser.parse_args()
 
 def main():
