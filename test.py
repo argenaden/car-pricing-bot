05c48d46f287587947882c8f94e5f9c1c88ba5f8
argen <argen.adenov@emoldino.com>
2023-12-04 19:45:18 +0900

    test parameter

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import requests
 import json
 
-def fetch_car_data(url, headers, cookies):
+def fetch_car_data(base_url, manufacturer, category, headers, cookies):
+    # Construct the query part of the URL
+    query = f"(And.Hidden.N._.(C.CarType.Y._.Manufacturer.{manufacturer}.)_.Category.{category}.)"
+    url = f"{base_url}?count=true&q={query}&sr=%7CModifiedDate%7C0%7C20"
+
     response = requests.get(url, headers=headers, cookies=cookies)
     if response.status_code == 200:
         return response.json()
@@ -30,7 +34,10 @@ def create_markdown_table(data, md_filename):
         file.write(md_table)
 
 def main():
-    url = 'http://api.encar.com/search/car/list/general?count=true&q=(And.(And.Hidden.N._.(C.CarType.Y._.(C.Manufacturer.%ED%98%84%EB%8C%80._.ModelGroup.%EA%B7%B8%EB%9E%9C%EC%A0%80.)))_.Trust.Warranty.)&sr=%7CExtendWarranty%7C0%7C5'
+    base_url = 'https://api.encar.com/search/car/list/premium'
+    manufacturer = "기아"
+    category = "경차"
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         'Accept': 'application/json, text/javascript, */*; q=0.01',
@@ -41,6 +48,7 @@ def main():
         'Origin': 'http://www.encar.com',
         'Referer': 'http://www.encar.com/',
     }
+
     cookies = {
         '_encar_hostname': 'http://www.encar.com',
         'cto_bundle': '1N4SLF9qdWE1Yk5hY3dicmtjeDZqWnFxcnFjd3BpRmVJSkMlMkZsdkV0azk4ZERYYU9ZdjNSSEtLaWFoTlhJbWwwSnBLVGoyMiUyRnI0NG82SnlHd3RpMG5oVXRxMGx3VXRMJTJCTzdvTkZBczQxVFJiZ1JCU3I1c2R4M1F5RWZ0QUp3WVVXRU1WaTE5N3oyJTJGRzRlbkFMd0NoJTJGcDI4REk3Skt4M2dUaXFpZjJ4ZzZDMkVXeGhRcVUlMkJpeXEwaHlLQUh1NjdFVmcxemtiMU1BcVgxV0k3dnVnJTJGYm5yQUhTWVElM0QlM0Q',
@@ -50,9 +58,9 @@ def main():
         'WMONID': '5bPyRCUPVzy'
     }
 
-    raw_data = fetch_car_data(url, headers, cookies)
+    raw_data = fetch_car_data(base_url, manufacturer, category, headers, cookies)
     if raw_data:
-        car_data = [ {
+        car_data = [{
             'Manufacturer': car.get('Manufacturer', ''),
             'Model': car.get('Model', ''),
             'Badge': car.get('Badge', ''),
@@ -69,11 +77,8 @@ def main():
 
         save_to_json(car_data, 'car_details.json')
 
-        json_filename = 'car_details.json'
-        md_filename = 'car_details.md'
-
-        car_data = load_json(json_filename)
-        create_markdown_table(car_data, md_filename)
+        car_data = load_json('car_details.json')
+        create_markdown_table(car_data, 'car_details.md')
 
 if __name__ == "__main__":
     main()
